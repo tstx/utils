26e1ecd0eee2d50e0f6170fff030f4732f8248ba
tstx <71097288+tstx@users.noreply.github.com>
2023-10-19 01:33:34 +0200

    Update clonehero_add_artistname_to_songfolders.py

--- clonehero_add_artistname_to_songfolders.py
+++ clonehero_add_artistname_to_songfolders.py
@@ -1,6 +1,10 @@
 import os
 
-# Usage: put this script next to Songs folder, start with DRY_RUN and check log file
+# Usage: put this script next to Songs folder, start with DRY_RUN and check log file.
+# This script adds the artist name to the front of each song's folder.
+# This makes it easier to get a good overview of your song collection 
+# and share specific songs with your picky friends.
+# Works on Windows, linux at own risk.
 
 # Set this to True to do a dry run (no files will be renamed)
 DRY_RUN = True
@@ -10,13 +14,14 @@ songs_path = "Songs"
 
 # Set up logging
 log_file = open("log.txt", "a")
+log_file.write(f"#####################################")
 
-def get_artist_name(file_path):
+def get_artist_name(ini_path):
     # Open the file and read its contents
     encodings = ["utf-8", "iso-8859-1", "cp1252"]
     for encoding in encodings:
         try:
-            with open(file_path, "r", encoding=encoding) as f:
+            with open(ini_path, "r", encoding=encoding) as f:
                 contents = f.read()
             break
         except UnicodeDecodeError:
@@ -34,7 +39,7 @@ def get_artist_name(file_path):
         if line.lower().startswith("artist="):
             # Extract the artist name from the line and return it
             artist_name = line.split("=")[1]
-            artist_name = artist_name.replace("/", "-")
+            artist_name = artist_name.replace("/", "-") # remove non filesystem characters
             return artist_name
 
     # If the artist name wasn't found, return None
@@ -46,27 +51,28 @@ for root, dirs, files in os.walk(songs_path):
         if file_name.lower() == "song.ini":
             folder_path = root
             # Read the song.ini file for the artist name
-            artist_name = get_artist_name(os.path.join(folder_path, "song.ini"))
+            artist_name = get_artist_name(os.path.join(folder_path, file_name))
 
             if artist_name is None:
-                print(f"artist not found for file: {folder_path}")
-                log_file.write(f"Couldn't find artist name for {folder_path}\n")
+                print(f"WARNING: artist not found for {folder_path}")
+                log_file.write(f"WARNING: artist not found for {folder_path}\n")
                 continue
 
             # Rename the folder with the artist name, if it doesn't already start with the artist name
             folder_name = os.path.basename(folder_path)
-            if not folder_name.lower().startswith(artist_name.lower()):
+            if folder_name.lower().startswith(artist_name.lower()):
+                log_file.write(f"SKIPPING: {folder_name} already starts with artist\n")
+            else:
                 new_folder_name = f"{artist_name} - {folder_name}"
                 if os.path.exists(os.path.join(songs_path, new_folder_name)):
-                    log_file.write(f"Skipping {folder_name} | {new_folder_name} already exists\n")
+                    log_file.write(f"SKIPPING: {folder_name} | {new_folder_name} already exists\n")
                 else:
                     print(os.path.join(folder_path, "song.ini"))
                     if DRY_RUN:
-                        # log_file.write(f"DRY RUN: {folder_name} | {new_folder_name}\n")
-                        log_file.write(f"DRY RUN: {folder_path} | {os.path.join(songs_path, new_folder_name)}\n")
+                        log_file.write(f"DRY RUN (RENAME): {folder_name} | {new_folder_name}\n")
                     else:
                         os.rename(folder_path, os.path.join(songs_path, new_folder_name))
-                        log_file.write(f"{folder_name} | {new_folder_name}\n")
+                        log_file.write(f"RENAMED: {folder_name} | {new_folder_name}\n")
 
 # Close the log file
 log_file.close()
